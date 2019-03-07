dab260f207140dd80c5558d9f7bcb1b7e86bcb5f
kaikaine <kairijb@protonmail.com>
2019-03-07 05:11:24 -0600

    created header

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Layout from './Hoc/Layout';
+
+const Routes = () => {
+  return ( 
+  <Layout>
+    Second line
+  </Layout> );
+}
+ 
+export default Routes;
