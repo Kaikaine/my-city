175caa33d8bcd287af06bda896ec4a59f0ecadad
kaikaine <kairijb@protonmail.com>
2019-03-08 17:58:42 -0600

    matches are now rendered onto screen

--- src/components/home/matches/Blocks.js
+++ src/components/home/matches/Blocks.js
@@ -1,35 +1,41 @@
-import React, { Component } from 'react'
-import {firebaseMatches} from '../../../firebase'
-import {firebaseLooper, reverseArr} from '../../utils/misc'
+import React, { Component } from "react";
+import { firebaseMatches } from "../../../firebase";
+import { firebaseLooper, reverseArr } from "../../utils/misc";
+import MatchesBlock from "./../../utils/matches_block";
 
 export default class Blocks extends Component {
-    state = {
-        matches:[]
-    }
+  state = {
+    matches: []
+  };
 
-    componentDidMount() {
-        firebaseMatches
-        .limitToLast(6)
-        .once('value')
-        .then((snapshot) => {
-            const matches = firebaseLooper(snapshot)
-            this.setState({matches: reverseArr(matches)})
-            console.log(this.state.matches)
-        })
-        .catch(err => console.log(err))
-    }
+  componentDidMount() {
+    firebaseMatches
+      .limitToLast(6)
+      .once("value")
+      .then(snapshot => {
+        const matches = firebaseLooper(snapshot);
+        this.setState({ matches: reverseArr(matches) });
+        console.log(this.state.matches);
+      })
+      .catch(err => console.log(err));
+  }
 
-    showMatches = () => {
-        return (<div>
-            match
-        </div>)
-    }
+  showMatches = matches =>
+    matches ? (
+      matches.map(match => (
+        <div key={match.id} className="item">
+          <div className="wrapper">
+            <MatchesBlock match={match} />
+          </div>
+        </div>
+      ))
+    ) : (
+      <div>No Matches</div>
+    );
 
   render() {
     return (
-      <div className='home_matches'>
-        {this.showMatches(this.state.matches)}
-      </div>
-    )
+      <div className="home_matches">{this.showMatches(this.state.matches)}</div>
+    );
   }
 }
