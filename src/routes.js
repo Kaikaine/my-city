fd49cbcab2f6d3efa14700b1e99c44f0596c7ffe
kaikaine <kairijb@protonmail.com>
2019-03-12 19:28:25 -0500

    added matches to admin area

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import Layout from './Hoc/Layout';
 import {Switch, Route} from 'react-router-dom'
+import PrivateRoute from './components/authRoutes/privateRoutes'
+import PublicRoute from './components/authRoutes/publicRoutes'
 import Home from './components/home/index';
 import SignIn from './components/signin/index';
 import Dashboard from './components/admin/Dashboard';
-import PrivateRoute from './components/authRoutes/privateRoutes'
-import PublicRoute from './components/authRoutes/publicRoutes'
+import AdminMatches from './components/admin/matches';
 
 const Routes = (props) => {
   return ( 
   <Layout>
     <Switch>
-
+      <PrivateRoute {...props} exact path='/admin_matches' component={AdminMatches}  />
       <PrivateRoute {...props} exact path='/dashboard' component={Dashboard}  />
       <PublicRoute {...props} restricted={true} path='/sign_in' component={SignIn} />
       <PublicRoute {...props} restricted={false} path='/' component={Home} />
